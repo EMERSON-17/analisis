528cc9270d1ffabd71ba1e9462a1ab3140e5453e
EMERSON-17 <italorl1755@gmail.com>
2024-10-22 13:11:00 -0500

    MI PRIMERA PRACTICA

---
+++ PRACTICA 1.js
@@ -0,0 +1,91 @@
+/*comentario largo*/ 
+//comentario corto//
+//una variable es para almacenar un tipo de dato//
+//unja constante un valor que no puede variar//
+//que es modulo el residuo//
+//concatenar es unir//
+//concatenacion implicita lo covierte encadena// 
+/*comentario largo*/
+//console.log (" HOLA " + " " + " MUNDO  " + "ERES EL REY" + " 36 ")
+//console.log(3+2)//5
+/*
+const readline = require('readline');  
+const rl = readline.createInterface({  
+    input: process.stdin,  
+    output: process.stdout  
+});  
+
+// Función para calcular el área de un cuadrado  
+function calcularAreaCuadrado(lado) {  
+    return lado * lado;  
+}  
+
+// Pedir la longitud del lado  
+rl.question('Ingrese la longitud del lado del cuadrado: ', (respuesta) => {  
+    const ladoNumero = parseFloat(respuesta);  
+
+    if (!isNaN(ladoNumero) && ladoNumero > 0) {  
+        const area = calcularAreaCuadrado(ladoNumero);  
+        console.log('El área del cuadrado es:', ${area});  
+    } else {  
+        console.log("Por favor, ingrese un número válido.");  
+    }  
+
+    // Cerrar la interfaz  
+    rl.close();  
+});
+*/
+
+/*triangulo
+let b = 5
+let l = 6
+resultado = (b*l)/2
+console.log('la respuesta es :',resultado)
+*/
+/*cuadrado
+let l1 = 5
+res = (l1*2)
+console.log('les respuesta es :',res)
+*/
+/*rectangulo
+let b = 2
+let al = 4
+res = (b*al)
+console.log('la resouesta es:' ,res)
+*/
+/*rombo
+let dma = 5
+let dme = 2
+res = (dma*dme)/2
+console.log('la respuesta es:', res)
+*/
+/*circulo
+let ra=5
+res=(ra*2)*3.1416
+console.log('la respuesta es:',res)
+*/
+
+function triangulo (b,a){
+    return rest = (b*a)/2
+}
+console.log(triangulo(5,6))
+
+function cuadrado(ld,l){
+    return rest = (ld*l)
+}
+console.log(cuadrado(5,6))
+
+function restangulo(ba,al){
+    return rest = (ba*al)
+}
+console.log(restangulo(5,6))
+
+function ronbo(dma,dme){
+    return rest = (dma*dme)/2
+}
+console.log(rombo(5,6))
+
+function circulo(ra){
+    return rest = (ra*2)*3.1416
+}
+console.log(circulo(5))
